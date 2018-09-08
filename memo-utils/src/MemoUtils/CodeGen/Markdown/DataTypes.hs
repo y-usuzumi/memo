a115f5a57446f04089b0f46e8588771737aff5c1
Yukio Usuzumi <anohigisavay@gmail.com>
2018-09-09 02:24:14 +0800

    Struggling with TH

--- memo-utils/src/MemoUtils/CodeGen/Markdown/DataTypes.hs
+++ memo-utils/src/MemoUtils/CodeGen/Markdown/DataTypes.hs
@@ -1,12 +1,16 @@
 module MemoUtils.CodeGen.Markdown.DataTypes where
 
-import           MemoUtils.DataTypes
+import           MemoUtils.DataTypes (TreeF, Tree)
+import           Text.Show.Deriving
 
 data TocItem = TocItem { title :: String
                        , link  :: String
                        } deriving Show
 
 type Toc = Tree TocItem
+
+$(deriveShow1 (TreeF TocItem))
+
 
 singletonToc :: TocItem -> Toc
 singletonToc item = Fix ( NodeF { node = item
--- memo-utils/src/MemoUtils/DataTypes.hs
+++ memo-utils/src/MemoUtils/DataTypes.hs
@@ -2,10 +2,12 @@ module MemoUtils.DataTypes ( module MemoUtils.DataTypes
                            , module Data.Functor.Foldable
                            ) where
 
+import           Data.Functor.Classes
 import           Data.Functor.Foldable
 
 data TreeF n r = NodeF { node     :: n
                        , children :: [r]
                        } deriving (Show, Functor)
 
-type Tree a = Fix (TreeF a)
+type Tree n = Fix (TreeF n)
+
