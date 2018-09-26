2fd4bead5c7fff1d1da37712a24bd640334d4006
Kenneth Zhao <kenneth.zhao@netis.com.cn>
2018-09-26 15:17:39 +0800

    Adjust sorting

--- memo-utils/src/MemoUtils/DataTypes.hs
+++ memo-utils/src/MemoUtils/DataTypes.hs
@@ -3,6 +3,7 @@ module MemoUtils.DataTypes where
 import           Control.Arrow
 import           Control.Monad
 import           Data.Functor.Foldable
+import           Data.List
 import           Data.Tree
 import           System.Directory
 import           System.FilePath.Posix
@@ -10,7 +11,7 @@ import           Text.Printf
 import           Text.Show.Deriving
 
 data TocItemType = Directory | File
-                 deriving (Enum, Eq, Show)
+                 deriving (Enum, Eq, Ord, Show)
 
 data TocItem = TocItem { title :: String
                        , link  :: String
@@ -34,10 +35,25 @@ andChildren toc children = Fix $
   (unfix toc){children=children
              }
 
+-- genToc :: FilePath -> IO Toc
+-- genToc fp = do
+--   isDir <- doesDirectoryExist fp
+--   let name = takeFileName fp
+--   let st = singletonToc $ TocItem { title = name
+--                                   , link = fp
+--                                   , type_ = if isDir then Directory else File
+--                                   }
+--   if isDir
+--     then do
+--     childTocs <- listDirectory fp >>= return . map (fp </>) >>= mapM genToc
+--     return $ st `andChildren` childTocs
+--     else
+--     return st
+
 genToc :: FilePath -> IO [Toc]
 genToc fp = do
   childFPs <- map (id &&& (fp </>)) <$> listDirectory fp
-  forM childFPs $ \(name, fullName) -> do
+  childTocs <- forM childFPs $ \(name, fullName) -> do
     let childFP = fp </> name
     isDir <- doesDirectoryExist childFP
     let st = singletonToc $ TocItem { title = name
@@ -47,6 +63,12 @@ genToc fp = do
     if isDir
       then do
       childTocs <- genToc childFP
-      return $ st `andChildren` childTocs
+      return $ st `andChildren` (sortBy sortFunc childTocs)
       else
       return st
+  return $ sortBy sortFunc childTocs
+  where
+    sortFunc (Fix (NodeF{ node = TocItem{ title = title1, type_ = t1 }})) (Fix (NodeF{ node = TocItem{ title = title2, type_ = t2 }}))
+      | t1 < t2 = GT
+      | t1 > t2 = LT
+      | t1 == t2 = compare title1 title2
